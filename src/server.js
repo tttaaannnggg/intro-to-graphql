5545ae65f4bd022c14fd44967668ebc4fedd3e79
tang <eytang8@gmail.com>
2019-09-26 18:45:17 -0700

    hi

--- src/server.js
+++ src/server.js
@@ -11,6 +11,12 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+    type Cat{
+      name: String
+    }
+    type Query{
+      myCat: Cat
+    }
     schema {
       query: Query
     }
@@ -19,7 +25,13 @@ export const start = async () => {
 
   const server = new ApolloServer({
     typeDefs: [rootSchema],
-    resolvers: {},
+    resolvers: {
+      Query: {
+        myCat() {
+          return { name: 'Garfield' }
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
